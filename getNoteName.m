7577849c1bb05e1dc248e894dc3272d5fab32805
Emma <Emma@phil.com>
2018-12-10 20:20:41 +0100

    removed skräp

--- getNoteName.m
+++ getNoteName.m
@@ -19,12 +19,10 @@ function noteName = getNoteName(centerYPos, rythm, stafflineYpos, spaceRadi)
         if(centerYPos(j) > stafflineYpos(5) + spaceRadi/2)
            
             % Calculate distance between lowest staff line and note center.
-            distanceFromLowestLine = centerYPos(j) - stafflineYpos(5);
-             
+            distanceFromLowestLine = centerYPos(j) - stafflineYpos(5); 
             % Use note head radius to calculate index. 
             noteIndex = firstLineArrayIndex - round(distanceFromLowestLine/spaceRadi); 
                 
-        
         %if note is above stafflines
         elseif(centerYPos(j) < stafflineYpos(1)- spaceRadi/2)
            
@@ -34,9 +32,9 @@ function noteName = getNoteName(centerYPos, rythm, stafflineYpos, spaceRadi)
             % Use note head radius to calculate index. 
             noteIndex = firstLineArrayIndex + 8 + round(distanceFromHighestLine/spaceRadi); 
              
-        %if note is in stafflines range
+        %*******if note is in stafflines range (more precise classification)
         else
-            %disp('in range')   
+               
             % calculate distance to staff lines
             d = abs(stafflineYpos-centerYPos(j));
             disp(d)
@@ -44,32 +42,26 @@ function noteName = getNoteName(centerYPos, rythm, stafflineYpos, spaceRadi)
 
             %if note is on staffline
             if(nearest < spaceRadi/2)
-                %disp('on line')
                  noteIndex = firstLineArrayIndex + (10 - 2*nearestIdx); 
+                 
             %if note is between 2 stafflines
             else
-                disp('in space')
+                
                 %if nearest is upper line
                 if( nearestIdx == 1)
-                    disp('upper space')
                     noteIndex = 13;
+                    
                 %if nearest is lower line
                 elseif( nearestIdx == 5)
-                    disp('lower space')
                     noteIndex = 7;
+                    
                 %if note is in space below nearest
                 elseif( d(nearestIdx-1) > d(nearestIdx+1) )
-                   fprintf( 'd to upper line: d% d to line below: %d \n', d(nearestIdx-1), d(nearestIdx+1))
-                   disp('below nearest')
                    noteIndex = firstLineArrayIndex + 2*(5 - (nearestIdx+0.5));
-                   disp(nearestIdx)
-                   fprintf('noteidx %d ', noteIndex)
+                   
                 %if note is in space above nearest
                 else
-                   fprintf( 'd to upper line: d% d to line below: %d \n', d(nearestIdx-1), d(nearestIdx+1))
-                   disp('above nearest')
                    noteIndex = firstLineArrayIndex + 2*(5 - (nearestIdx-0.5));
-                   fprintf('noteidx %d', noteIndex)
                 end
             end
         end
